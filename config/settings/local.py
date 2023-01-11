591b919f767b1ddc0e313f9379865d1740dcc809
luigi617 <luigiliu617@gmail.com>
2023-01-11 04:57:01 +0100

    change

--- config/settings/local.py
+++ config/settings/local.py
@@ -23,5 +23,5 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
 
-# ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+# ALLOWED_HOSTS = ["*"]
