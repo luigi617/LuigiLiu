5bc313fe458ad74f104de39ee35c0e9d6989f04c
luigi617 <luigiliu617@gmail.com>
2023-01-11 04:19:03 +0100

    bugs

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# KnowledgExchange
+# LuigiLiu
--- config/asgi.py
+++ config/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for KnowledgExchange project.
+ASGI config for LuigiLiu project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
