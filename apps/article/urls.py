f368002bbf2e1a1e8dacbed9bab423f88d79fb6a
luigi617 <luigiliu617@gmail.com>
2023-01-15 01:42:52 +0100

    article

--- apps/article/urls.py
+++ apps/article/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.articles, name='articles'),
     path('<int:pk>/', views.article, name='article'),
     path('create/', views.create_article, name='create-article'),
+    path('edit/<int:pk>/', views.edit_article, name='edit-article'),
 
 ]
