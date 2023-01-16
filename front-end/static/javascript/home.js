46be925f78f313680410e32a2e321411b5b7f89f
luigi617 <luigiliu617@gmail.com>
2023-01-16 15:08:19 +0100

    home canva

--- front-end/static/javascript/home.js
+++ front-end/static/javascript/home.js
@@ -42,109 +42,124 @@ APP['home'] = {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight-72;
     
+        function dot_num() {
+            return Math.ceil((canvas.width * canvas.height) * 100/1077120)
+        }
+        
 
-
-        var stars = [], // Array that contains the stars
-            FPS = 60, // Frames per second
-            x = 100, // Number of stars
-            mouse = {
+        var stars = []; // Array that contains the stars
+        FPS = 120; // Frames per second
+        x = dot_num(); // Number of stars
+        mouse = {
             x: 0,
             y: 0
-            };  // mouse location
+        };  // mouse location
 
-        // Push stars to array
-
-        for (var i = 0; i < x; i++) {
-        stars.push({
-            x: Math.random() * canvas.width,
-            y: Math.random() * canvas.height,
-            radius: Math.random() * 1 + 1,
-            vx: Math.floor(Math.random() * 50) - 25,
-            vy: Math.floor(Math.random() * 50) - 25
-        });
+        
+        function init() {
+            // Push stars to array
+            x = dot_num();
+            stars = [];
+            for (var i = 0; i < x; i++) {
+                stars.push({
+                    x: Math.random() * canvas.width,
+                    y: Math.random() * canvas.height,
+                    radius: Math.random() * 1 + 1,
+                    vx: Math.floor(Math.random() * 50) - 25,
+                    vy: Math.floor(Math.random() * 50) - 25
+                });
+            }
         }
 
         // Draw the scene
 
         function draw() {
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-        
-        ctx.globalCompositeOperation = "lighter";
-        
-        for (var i = 0, x = stars.length; i < x; i++) {
-            var s = stars[i];
-        
-            ctx.fillStyle = "#fff";
+            ctx.clearRect(0,0,canvas.width,canvas.height);
+            
+            ctx.globalCompositeOperation = "lighter";
+            
+            for (var i = 0, x = stars.length; i < x; i++) {
+                var s = stars[i];
+            
+                ctx.fillStyle = "#fff";
+                ctx.beginPath();
+                ctx.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
+                ctx.fill();
+                ctx.fillStyle = 'black';
+                ctx.stroke();
+            }
+            
             ctx.beginPath();
-            ctx.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
-            ctx.fill();
-            ctx.fillStyle = 'black';
+            for (var i = 0, x = stars.length; i < x; i++) {
+                var starI = stars[i];
+                ctx.moveTo(starI.x,starI.y); 
+                if(distance(mouse, starI) < 150) ctx.lineTo(mouse.x, mouse.y-72+window.scrollY);
+                for (var j = 0, x = stars.length; j < x; j++) {
+                    var starII = stars[j];
+                    if(distance(starI, starII) < 150) {
+                        //ctx.globalAlpha = (1 / 150 * distance(starI, starII).toFixed(1));
+                        ctx.lineTo(starII.x,starII.y); 
+                    }
+                }
+            }
+            ctx.lineWidth = 0.1;
+            ctx.strokeStyle = 'white';
             ctx.stroke();
-        }
-        
-        ctx.beginPath();
-        for (var i = 0, x = stars.length; i < x; i++) {
-            var starI = stars[i];
-            ctx.moveTo(starI.x,starI.y); 
-            if(distance(mouse, starI) < 150) ctx.lineTo(mouse.x, mouse.y-72+window.scrollY);
-            for (var j = 0, x = stars.length; j < x; j++) {
-            var starII = stars[j];
-            if(distance(starI, starII) < 150) {
-                //ctx.globalAlpha = (1 / 150 * distance(starI, starII).toFixed(1));
-                ctx.lineTo(starII.x,starII.y); 
             }
-            }
-        }
-        ctx.lineWidth = 0.1;
-        ctx.strokeStyle = 'white';
-        ctx.stroke();
-        }
 
-        function distance( point1, point2 ){
-        var xs = 0;
-        var ys = 0;
-        
-        xs = point2.x - point1.x;
-        xs = xs * xs;
-        
-        ys = point2.y - point1.y;
-        ys = ys * ys;
-        
-        return Math.sqrt( xs + ys );
+            function distance( point1, point2 ){
+            var xs = 0;
+            var ys = 0;
+            
+            xs = point2.x - point1.x;
+            xs = xs * xs;
+            
+            ys = point2.y - point1.y;
+            ys = ys * ys;
+            
+            return Math.sqrt( xs + ys );
         }
 
         // Update star locations
 
         function update() {
-        for (var i = 0, x = stars.length; i < x; i++) {
-            var s = stars[i];
-        
-            s.x += s.vx / FPS;
-            s.y += s.vy / FPS;
+            for (var i = 0, x = stars.length; i < x; i++) {
+                var s = stars[i];
             
-            if (s.x < 0 || s.x > canvas.width) s.vx = -s.vx;
-            if (s.y < 0 || s.y > canvas.height) s.vy = -s.vy;
-        }
+                s.x += s.vx / FPS;
+                s.y += s.vy / FPS;
+                
+                if (s.x < 0 || s.x > canvas.width) s.vx = -s.vx;
+                if (s.y < 0 || s.y > canvas.height) s.vy = -s.vy;
+            }
         }
 
         canvas.addEventListener('mousemove', function(e){
-        mouse.x = e.clientX;
-        mouse.y = e.clientY;
+            mouse.x = e.clientX;
+            mouse.y = e.clientY;
         });
 
         // Update and draw
-
+        var requestId;
         function tick() {
-        draw();
-        update();
-        requestAnimationFrame(tick);
+            draw();
+            update();
+            requestId = requestAnimationFrame(tick);
         }
-
+        init();
         tick();
+        var resizeTimer;
         window.addEventListener('resize', function(event) {
-            canvas.width = window.innerWidth;
-            canvas.height = window.innerHeight-72;
-            canvas.dispatchEvent(new Event('mousemove'));
+            clearTimeout(resizeTimer);
+            resizeTimer = setTimeout(function() {
+                // Run code here, resizing has "stopped"      
+                canvas.width = window.innerWidth;
+                canvas.height = window.innerHeight-72;
+                console.log(requestId)
+                cancelAnimationFrame(requestId);
+                init();
+                tick();
+            }, 50);
         }, true);
     },
 }
