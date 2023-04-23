f67eabfbb15ea96c4a75d8e62e08e268f98bdf2e
luigi617 <luigiliu617@gmail.com>
2023-04-23 11:46:34 +0200

    size hint

--- front-end/static/javascript/base.js
+++ front-end/static/javascript/base.js
@@ -99,6 +99,24 @@ APP['base'] = {
         }
         return cookieValue;
     },
+    "calculate_size": function calculateSize(img, maxWidth, maxHeight) {
+        let width = img.width;
+        let height = img.height;
+
+        // calculate the width and height, constraining the proportions
+        if (width > height) {
+            if (width > maxWidth) {
+            height = Math.round((height * maxWidth) / width);
+            width = maxWidth;
+            }
+        } else {
+            if (height > maxHeight) {
+            width = Math.round((width * maxHeight) / height);
+            height = maxHeight;
+            }
+        }
+        return [width, height];
+    },
 }
 
 
