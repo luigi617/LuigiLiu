9191492503140f5de37f183b0945300d221f54d0
luigi <luigiliu617@gmail.com>
2023-11-13 18:02:03 +0100

    d

--- certbot/certify-init.sh
+++ certbot/certify-init.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo "Getting certificatee..."
+# echo "Getting certificatee..."
 
 until nc -z nginx 80; do
     echo "Waiting for nginx..."
@@ -11,4 +11,11 @@ done
 
 echo "Getting certificate..."
 
-# certbot certonly --webroot --webroot-path /code/data/certbot/www/ -d luigiliu.com --email luigiliu617@gmail.com --rsa-key-size 4096 --agree-tos --noninteractive
+certbot certonly \
+    --webroot \
+    --webroot-path "/code/data/certbot/www/" \
+    -d "$DOMAIN" \
+    --email $EMAIL \
+    --rsa-key-size 4096 \
+    --agree-tos \
+    --noninteractive
