acf5c691bcc636cbaa3f65d0b6f578fb0358503d
luigi617 <luigiliu617@gmail.com>
2024-06-26 16:56:00 +0200

    file size

---
+++ apps/api/custom_api/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from apps.map.models import Marker
+from apps.user.models import User
+from apps.api.users.serializers import BasicUserSerializer
+from config.settings.base import MEDIA_URL
+
--- apps/api/urls.py
+++ apps/api/urls.py
@@ -4,6 +4,7 @@ from apps.api.home.urls import home_urlpatterns
 from apps.api.dictionary.urls import dictionary_urlpatterns
 from apps.api.treasure_hunt.urls import treasure_hunt_urlpatterns
 from apps.api.map.urls import map_urlpatterns
+from apps.api.custom_api.urls import custom_api_urlpatterns
 from rest_framework.urlpatterns import format_suffix_patterns
 
 
@@ -13,5 +14,6 @@ urlpatterns = format_suffix_patterns(
     home_urlpatterns +
     dictionary_urlpatterns +
     treasure_hunt_urlpatterns +
-    map_urlpatterns
+    map_urlpatterns +
+    custom_api_urlpatterns
 )
